ab8d00b92b953df4c98acccf67aeb9ca6478f694
Vince Angelo Batecan <llTheBlankll@gmail.com>
2025-06-14 23:25:58 +0800

    feat(security): add UserDetailsService, provider & config improvements (#master)
    - Add UserAccountService-based UserDetailsService bean
    - Add AuthenticationProvider and AuthenticationManager beans
    - Inject driver and dialect properties for PostgreSQL in application.properties
    - Switch JWT secret and expiration to environment variable interpolation
    - Prepare for improved authentication and configuration flexibility

--- src/main/java/com/vbatecan/patient_management_system/config/SecurityConfig.java
+++ src/main/java/com/vbatecan/patient_management_system/config/SecurityConfig.java
@@ -1,15 +1,46 @@
 package com.vbatecan.patient_management_system.config;
 
+import com.vbatecan.patient_management_system.service.interfaces.UserAccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserAccountService userAccountService;;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username ->userAccountService.findByUsername(username).orElseThrow(
+          () -> new UsernameNotFoundException("User not found with username: " + username)
+        );
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 }
